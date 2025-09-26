87a021adff949c4d24655c350f8eb5b1de875058
rjrizani <rjrizani@gmail.com>
2025-09-26 16:12:23 +0800

    update config

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// GANTI DENGAN KONFIGURASI PROYEK FIREBASE ANDA
-// Anda bisa mendapatkan ini dari Firebase Console:
-// Project Settings > General > Your apps > Web app > Firebase SDK snippet > Config
-
 const firebaseConfig = {
     apiKey: "AIzaSyCDGI7HCnkzuecq7KHZB2F7k8sLHAEjFyQ",
     authDomain: "points-of-sale-f20ae.firebaseapp.com",
@@ -11,11 +7,3 @@ const firebaseConfig = {
     appId: "1:244013009855:web:1ee550a3017e7ae0472f0a",
     databaseURL: "https://points-of-sale-f20ae-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
-
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Export Firebase services
-const db = firebase.database();
-const auth = firebase.auth();
